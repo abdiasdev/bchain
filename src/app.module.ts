9ccb53e6c57ac6e337f04e4ca5f47f87f3613acc
abdias.dev <abdias.dev@gmail.com>
2022-05-19 02:10:36 +0200

    Added TypeORM conn Params

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'abdias',
+      password: 'lizzie',
+      database: 'bchain',
+      "entities": ["dist/**/*.entity{.ts,.js}"],
+      synchronize: false,
+      retryDelay: 3000,
+      retryAttempts: 10
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
